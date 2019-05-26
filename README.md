fcfa74056ba6e2bd79875031288e6deaa13d5e11
Shrinath Nimare <shrinath.nimare@gmail.com>
2019-05-27 01:40:58 +0530

    Working on file db

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # MediaServerDirGen
-A set of scripts to generate make your normal media directory into an HTTP media server
+
+## What is this?
+A set of scripts to convert any normal media library, say your precious movies or music collection that lives on your external USB harddrive into an HTTP server ready format.
+Think Kodi, but for the browser.
+
+## How does it do that?
+* It indexes every single media file in the directories you give it, and all of their subdirectories and creates nice HTML pages all with nice video tags for audio and video and images for, of course, images.
+
+## What else?
+* This is so that any normal HTTP server can host your media library and it can be accessible from any device on your network or even outside if you configure everything correctly.
+* Directory listing is similar but do you REALLY not want a specialized software for EVERYTHING?
+
+## Future plans
+* Custom CSS support
+* Basic features like media randomization, photoframe mode, etc
+* Cover art, lyrics fetching for music
+* Plot description, rating, synopsis, cover art fetching for movies, TV shows, and anime
+* Visual configuration
+
+## How much of this is done?
+- [ ] Basic functionality
+  - [ ] Directory Listing
+  - [ ] HTML page generation
+  - [ ] Custom CSS
+  - [ ] Photoframe Mode
+- [ ] Music related
+  - [ ] Artist Info
+  - [ ] Covert Art
+  - [ ] Lyrics
+- [ ] TV Shows, video, anime related
+  - [ ] IMDb info (description, rating, etc)
+  - [ ] Covert Art
--- msdr_filedb.py
+++ msdr_filedb.py
@@ -1,43 +1,75 @@
 #!/bin/python
 
-def get_music_dir_list():
-	music_list = []
-	filename = open('music_dir_list', 'r') #opening file
-	read_string = filename.readline() #reading the first line
-	while(read_string != ''): #looping all lines
-		if(read_string[0] != '#'): #not a comment
-			music_list.append(read_string)
-		read_string = filename.readline()
-	filename.close()
-	return music_list
-	
-def get_movie_dir_list():
-	movie_list = []
-	filename = open('movie_dir_list', 'r') #opening file
-	read_string = filename.readline() #reading the first line
-	while(read_string != ''): #looping all lines
-		if(read_string[0] != '#'): #not a comment
-			movie_list.append(read_string)
-		read_string = filename.readline()
-	filename.close()
-	return movie_list
+#this function will create the config file when it is run
+def create_config_file():
+	fil = open('msdr.conf', 'w')
+	#the below is the text to write in the newly created config file
+	#basic help text is added for understanding of the config file
+	text_to_write = '''###
+# Welcome to MSDR config file
+# Editing this file is very simple
+# Lines starting with a '#' are comments and are ignored by the program
+#
+###DIRECTORIES###
+# There are 5 top level directory types
+#
+# 1 - MUS, music directories, subdirectories must follow the following structure
+# Artist->Album->Audio_File
+#
+# 2 - VID, video directories, will include all subdirectories, and all files
+#
+# 3 - MOV, movie directories, subdirectories must follow the following structure
+# Movie_Name->Video_File
+#
+# 4 - TVS, TV show directories, subdirectories must follow the followind structure
+# TV_Show_Name->Episode_Files
+#
+# 5 - PHO, Photo directories, will include all subdirectories, and all files
+#
+# For example, to add a video directory, including subdirectories, add the following
+# VID,/path/to/video/directory
+# For example, to add a photo directory, including subdirectories, add the following
+# PHO,/path/to/video/directory
+###
+'''
+	fil.write(text_to_write)
+	fil.close()
 
-def get_tv_dir_list():
-	tv_list = []
-	filename = open('tv_dir_list.txt', 'r') #opening file
-	read_string = filename.readline() #reading the first line
-	while(read_string != ''): #looping all lines
-		if(read_string[0] != '#'): #not a comment
-			tv_list.append(str(read_string))
-		read_string = filename.readline()			
-	return tv_list
+#this function returns a list of all the directories in the config file
 
-def get_images_dir_list():
-	images_list = []
-	filename = open('images_dir_list', 'r') #opening file
+def get_photo_list(photo_directories):
+	try:
+		subdirs = os.listdir(photo_directories)
+		for i in subdirs:
+			os.chdir(i)
+			output = get_photo_list(i)
+			os.chdir('..')
+		return output
+	except NotADirectoryError:
+		if(i[-4:] == '.jpg' or i[-5:] == '.jpeg' or i[-4:] == '.gif' or i[-4:] == '.png' or i[-4:] == '.bmp'):
+			return 
+
+def get_dir_list_from_config():
+	music_dir = [] #MUS
+	video_dir = [] #VID
+	movie_dir = [] #MOV
+	tvsho_dir = [] #TVS
+	photo_dir = [] #PHO
+	filename = open('msdr.conf', 'r') #opening file
 	read_string = filename.readline() #reading the first line
 	while(read_string != ''): #looping all lines
 		if(read_string[0] != '#'): #not a comment
-			images_list.append(read_string)
-		read_string = filename.readline()			
-	return images_list
+			if(read_string[0:3] == 'MUS'): #music directory
+				music_dir.append(read_string[4:])
+			elif(read_string[0:3] == 'VID'): #video directory
+				video_dir.append(read_string[4:])
+			elif(read_string[0:3] == 'MOV'): #movie directory
+				movie_dir.append(read_string[4:])
+			elif(read_string[0:3] == 'TVS'): #tv show directory
+				tvsho_dir.append(read_string[4:])
+			elif(read_string[0:3] == 'PHO'): #photos directory
+				photo_dir.append(read_string[4:])
+		read_string = filename.readline() #read new line
+	filename.close() #finally close the file
+	#return all the different directory lists
+	return (music_dir, video_dir, movie_dir, tvsho_dir, photo_dir)
